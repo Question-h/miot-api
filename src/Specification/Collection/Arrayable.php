8a54fba30fa67400a17a06c666d957a9e8ba4853
lixiaodong <lixiaodong@yeelight.com>
2018-06-08 15:00:05 +0800

    引入laravel的collection操作类

---
+++ src/Specification/Collection/Arrayable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Yeelight\Specification\Collection;
+
+interface Arrayable
+{
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray();
+}
